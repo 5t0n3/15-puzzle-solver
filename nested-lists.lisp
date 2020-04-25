54c225d9dd46af74ec1a88ab371f0a554943ee3d
Zane Othman-Gomez <1nk5t0n3@gmail.com>
2020-04-24 17:15:50 -0700

    Replaced copy-list with copy-tree in swap-items-nested.
    This leads to a deep copy of the list (I think), whereas copy-list
    only copies the structure of the list (modifications to either the
    copy or the original modify both).
    
    Also removed some debugging format statements from working functions.

--- nested-lists.lisp
+++ nested-lists.lisp
@@ -20,18 +20,14 @@
 
 (defun (setf nested-nth) (new-value index-list nested-list)
   (let ((replace-row (get-row index-list nested-list)))
-    (setf (nth (car index-list) replace-row) new-value)
-    (format t "New state: ~a~%" nested-list)
-    (format t "Replaced row: ~a" replace-row)))
+    (setf (nth (car index-list) replace-row) new-value)))
 
-;; TODO: I can't setf with nested-nth, so I need to figure out either how to be able to
-;; or use a different function (like setfing the row instead of the item, which I think would work)l
 (defun swap-items-nested (original-list first-item second-item)
   "Non-destructively swaps the two given items in original-list, which must be a list of lists."
   (let ((first-idx (find-nested-index original-list first-item))
         (second-idx (find-nested-index original-list second-item))
-        (copied-list (copy-list original-list)))
+        (copied-list (copy-tree original-list))) ; Note that copy-tree is used to avoid shared structure
     (setf (nested-nth first-idx copied-list) second-item)
     (setf (nested-nth second-idx copied-list) first-item)
-    (format t "Original list: ~a~%~%New list: ~a~%" original-list copied-list)))
+    copied-list))
 
